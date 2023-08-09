31e45a331748d3dbe7ec8b40be740ba78c1bff54
Kartofel-2834 <poko2834@mail.ru>
2023-08-09 10:49:16 +0300

    Added intro component

---
+++ src/components/page-intro.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="intro">
+    <img src="/intro.jpg" alt="intro" class="intro__background" />
+
+    <section class="intro__content">
+      <slot />
+    </section>
+  </div>
+</template>
+
+<style>
+.intro {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr;
+  align-items: end;
+
+  max-width: 100%;
+  overflow: hidden;
+}
+
+.intro > * {
+  grid-column: 1 / 2;
+  grid-row: 1 / 2;
+}
+
+.intro__background,
+.intro__content {
+  width: 100%;
+}
+</style>
