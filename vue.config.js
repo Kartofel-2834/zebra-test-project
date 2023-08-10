ae3cbb1085e0b83fa82a4eec97d11acbd7935a17
Kartofel-2834 <kanalori@mail.ru>
2023-08-10 16:06:51 +0300

    Added link for github pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: 'https://kartofel-2834.github.io/zebra-test-project/',
 });
