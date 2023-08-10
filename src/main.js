a6237e4da4c632d970e440e2fce59f72dceb542f
Kartofel-2834 <kanalori@mail.ru>
2023-08-10 15:50:53 +0300

    Build setup

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from "vue";
-import App from "./app.vue";
-
-const appInstance = createApp(App);
-
-appInstance.mount("#app");
--- src/utils/sleep.js
+++
@@ -1,3 +0,0 @@
-export default async function sleep(milliseconds) {
-  return new Promise((res) => setTimeout(res, milliseconds));
-}
